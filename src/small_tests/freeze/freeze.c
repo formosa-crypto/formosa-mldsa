98fbc39a9f814b44c539f68d2117e793c9b4eed8
Tiago Oliveira <tfaoliveira@gmail.com>
2023-09-26 16:34:26 +0200

    draft of freeze; reduce.c done

---
+++ src/small_tests/freeze/freeze.c
@@ -0,0 +1,52 @@
+#include <stdint.h>
+
+#define Q 8380417
+
+/*************************************************
+* Name:        reduce32
+*
+* Description: For finite field element a with a <= 2^{31} - 2^{22} - 1,
+*              compute r \equiv a (mod Q) such that -6283009 <= r <= 6283007.
+*
+* Arguments:   - int32_t: finite field element a
+*
+* Returns r.
+**************************************************/
+int32_t reduce32(int32_t a) {
+    int32_t t;
+
+    t = (a + (1 << 22)) >> 23;
+    t = a - t * Q;
+    return t;
+}
+
+/*************************************************
+* Name:        caddq
+*
+* Description: Add Q if input coefficient is negative.
+*
+* Arguments:   - int32_t: finite field element a
+*
+* Returns r.
+**************************************************/
+int32_t caddq(int32_t a) {
+    a += (a >> 31) & Q;
+    return a;
+}
+
+/*************************************************
+* Name:        freeze
+*
+* Description: For finite field element a, compute standard
+*              representative r = a mod^+ Q.
+*
+* Arguments:   - int32_t: finite field element a
+*
+* Returns r.
+**************************************************/
+int32_t freeze(int32_t a) {
+    a = reduce32(a);
+    a = caddq(a);
+    return a;
+}
+
