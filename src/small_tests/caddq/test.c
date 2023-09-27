4908f022d64243994e5d47fe41b39f9b611be81a
Jean-Christophe Léchenet <jean-christophe.lechenet@inria.fr>
2023-09-27 10:53:09 +0200

    First test

---
+++ src/small_tests/caddq/test.c
@@ -0,0 +1,32 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../randombytes/randombytes.h"
+
+#define TESTS 20
+
+int32_t caddq(int32_t a);
+int32_t caddq_jazz(int32_t a);
+
+int main () {
+  int32_t arg;
+
+  for(int t=0; t<TESTS; t++) {
+    // initialize 4 bytes of randomness
+    randombytes((uint8_t*)(&arg), sizeof(int32_t));
+
+    int32_t res_c = caddq(arg);
+    int32_t res_jazz = caddq_jazz(arg);
+
+    if (res_c != res_jazz) {
+      printf("%d -> %d != %d\n", arg, res_c, res_jazz);
+      printf("FAIL: caddq\n");
+      exit(1);
+    }
+  }
+
+  printf("PASS: caddq\n");
+
+  return 0;
+}
