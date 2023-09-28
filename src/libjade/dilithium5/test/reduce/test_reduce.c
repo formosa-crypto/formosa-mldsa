bd40497a3ac9c481af4018e1cfa3278320745855
Tiago Oliveira <tfaoliveira@gmail.com>
2023-09-28 16:43:30 +0200

    setting up long term dev. structure: WIP.

---
+++ src/libjade/dilithium5/test/reduce/test_reduce.c
@@ -0,0 +1,43 @@
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "notrandombytes.c"
+
+#ifndef TESTS
+#define TESTS 1000
+#endif
+
+int32_t PQCLEAN_DILITHIUM5_CLEAN_montgomery_reduce(int64_t a);
+int32_t PQCLEAN_DILITHIUM5_CLEAN_montgomery_reduce_jazz(int64_t a);
+
+int test_montgomery_reduce()
+{
+  int64_t arg;
+
+  for(int t=0; t<TESTS; t++)
+  {
+    // initialize 8 bytes of randomness
+    randombytes((uint8_t*)(&arg), sizeof(int64_t));
+
+    int32_t res_c = PQCLEAN_DILITHIUM5_CLEAN_montgomery_reduce(arg);
+    int32_t res_jazz = PQCLEAN_DILITHIUM5_CLEAN_montgomery_reduce_jazz(arg);
+
+    if (res_c != res_jazz) {
+      printf("%" PRId64 " -> %" PRId32 " != %" PRId32 "\n", arg, res_c, res_jazz);
+      printf("FAIL: montgomery_reduce\n");
+      exit(1);
+    }
+  }
+
+  printf("PASS: montgomery_reduce\n");
+
+  return 0;
+}
+
+int main ()
+{
+  test_montgomery_reduce();
+  return 0;
+}
+
