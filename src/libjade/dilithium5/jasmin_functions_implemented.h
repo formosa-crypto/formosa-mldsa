c6b2988689f9074c3161ee2f28e0395958ca68cd
Benjamin Gregoire <benjamin.gregoire@inria.fr>
2023-12-16 15:04:46 +0100

    polyeta_unpack

--- src/libjade/dilithium5/jasmin_functions_implemented.h
+++ src/libjade/dilithium5/jasmin_functions_implemented.h
@@ -74,14 +74,13 @@
 #define poly_decompose_jazz
 #define PQCLEAN_DILITHIUM5_CLEAN_poly_decompose PQCLEAN_DILITHIUM5_CLEAN_poly_decompose_jazz
 
-
 #define polyeta_pack_jazz
 #define PQCLEAN_DILITHIUM5_CLEAN_polyeta_pack PQCLEAN_DILITHIUM5_CLEAN_polyeta_pack_jazz
 
-#if 0
 #define polyeta_unpack_jazz
 #define PQCLEAN_DILITHIUM5_CLEAN_polyeta_unpack PQCLEAN_DILITHIUM5_CLEAN_polyeta_unpack_jazz
 
+#if 0
 #define poly_invntt_tomont_jazz
 #define PQCLEAN_DILITHIUM5_CLEAN_poly_invntt_tomont PQCLEAN_DILITHIUM5_CLEAN_poly_invntt_tomont_jazz
 
