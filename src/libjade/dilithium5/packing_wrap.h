d12d8051eea63bb9532a61ff0230be43242c0fec
Côme Le Breton <come.le-breton@inria.fr>
2024-06-13 23:08:29 +0200

    Advance on packing impl.
    unpack sk to impl.

---
+++ src/libjade/dilithium5/packing_wrap.h
@@ -0,0 +1,40 @@
+#ifndef PQCLEAN_DILITHIUM5_CLEAN_PACKING_WRAP_H
+#define PQCLEAN_DILITHIUM5_CLEAN_PACKING_WRAP_H
+#include "params.h"
+#include "packing.h"
+#include <stdint.h>
+
+void PQCLEAN_DILITHIUM5_CLEAN_pack_sk_internal_jazz(uint8_t sk[PQCLEAN_DILITHIUM5_CLEAN_CRYPTO_SECRETKEYBYTES], const uint8_t* rho_tr_key[3], const int32_t* to_s1_s2[3]);
+
+void PQCLEAN_DILITHIUM5_CLEAN_pack_sk_jazz(uint8_t sk[PQCLEAN_DILITHIUM5_CLEAN_CRYPTO_SECRETKEYBYTES], const uint8_t rho[SEEDBYTES], const uint8_t tr[SEEDBYTES], const uint8_t key[SEEDBYTES], const polyveck *t0, const polyvecl *s1, const polyveck *s2);
+void PQCLEAN_DILITHIUM5_CLEAN_unpack_sk_jazz(uint8_t rho[SEEDBYTES], uint8_t tr[SEEDBYTES], uint8_t key[SEEDBYTES], polyveck *t0, polyvecl *s1, polyveck *s2,const uint8_t sk[PQCLEAN_DILITHIUM5_CLEAN_CRYPTO_SECRETKEYBYTES]);
+
+void PQCLEAN_DILITHIUM5_CLEAN_pack_sk_jazz(uint8_t sk[PQCLEAN_DILITHIUM5_CLEAN_CRYPTO_SECRETKEYBYTES], const uint8_t rho[SEEDBYTES], const uint8_t tr[SEEDBYTES], const uint8_t key[SEEDBYTES], const polyveck *t0, const polyvecl *s1, const polyveck *s2) {
+  const uint8_t* rho_tr_key[3];
+  rho_tr_key[0] = rho;
+  rho_tr_key[1] = tr;
+  rho_tr_key[2] = key;
+
+  const int32_t* t0_s1_s2[3];
+  t0_s1_s2[0] = t0;
+  t0_s1_s2[1] = s1;
+  t0_s1_s2[2] = s2;
+
+  PQCLEAN_DILITHIUM5_CLEAN_pack_sk_internal_jazz(sk, rho_tr_key, t0_s1_s2);
+}
+
+void PQCLEAN_DILITHIUM5_CLEAN_unpack_sk_jazz(uint8_t rho[SEEDBYTES], uint8_t tr[SEEDBYTES], uint8_t key[SEEDBYTES], polyveck *t0, polyvecl *s1, polyveck *s2,const uint8_t sk[PQCLEAN_DILITHIUM5_CLEAN_CRYPTO_SECRETKEYBYTES]) {
+   const uint8_t* rho_tr_key[3];
+  rho_tr_key[0] = rho;
+  rho_tr_key[1] = tr;
+  rho_tr_key[2] = key;
+
+  const int32_t* t0_s1_s2[3];
+  t0_s1_s2[0] = t0;
+  t0_s1_s2[1] = s1;
+  t0_s1_s2[2] = s2;
+
+  PQCLEAN_DILITHIUM5_CLEAN_unpack_sk_internal_jazz(rho_tr_key, t0_s1_s2, sk); 
+}
+
+#endif
--- src/libjade/dilithium5/test/test_packing.c
+++ src/libjade/dilithium5/test/test_packing.c
@@ -14,20 +14,20 @@
 #define TESTS 1000
 #endif
 
+#include "packing_wrap.h"
+
 void PQCLEAN_DILITHIUM5_CLEAN_pack_pk_jazz(uint8_t pk[PQCLEAN_DILITHIUM5_CLEAN_CRYPTO_PUBLICKEYBYTES], const uint8_t rho[SEEDBYTES], const polyveck *t1);
 void PQCLEAN_DILITHIUM5_CLEAN_unpack_pk_jazz(uint8_t rho[SEEDBYTES], polyveck *t1, const uint8_t pk[PQCLEAN_DILITHIUM5_CLEAN_CRYPTO_PUBLICKEYBYTES]);
-void PQCLEAN_DILITHIUM5_CLEAN_pack_sk_jazz(uint8_t sk[PQCLEAN_DILITHIUM5_CLEAN_CRYPTO_SECRETKEYBYTES], const uint8_t rho[SEEDBYTES], const uint8_t tr[SEEDBYTES], const uint8_t key[SEEDBYTES], const polyveck *t0, const polyvecl *s1, const polyveck *s2);
-void PQCLEAN_DILITHIUM5_CLEAN_unpack_sk_jazz(uint8_t rho[SEEDBYTES], uint8_t tr[SEEDBYTES], uint8_t key[SEEDBYTES], polyveck *t0, polyvecl *s1, polyveck *s2,const uint8_t sk[PQCLEAN_DILITHIUM5_CLEAN_CRYPTO_SECRETKEYBYTES]);
 void PQCLEAN_DILITHIUM5_CLEAN_pack_sig_jazz(uint8_t sig[PQCLEAN_DILITHIUM5_CLEAN_CRYPTO_BYTES], const uint8_t c[SEEDBYTES], const polyvecl *z, const polyveck *h);
 int PQCLEAN_DILITHIUM5_CLEAN_unpack_sig_jazz(uint8_t c[SEEDBYTES], polyvecl *z, polyveck *h, const uint8_t sig[PQCLEAN_DILITHIUM5_CLEAN_CRYPTO_BYTES]);
 
 int main ()
 {
-    check_pk_rhot1(PQCLEAN_DILITHIUM5_CLEAN_pack_pk_jazz, PQCLEAN_DILITHIUM5_CLEAN_pack_pk, "_pack_pk_jazz");
-    check_rhot1_pk(PQCLEAN_DILITHIUM5_CLEAN_unpack_pk_jazz, PQCLEAN_DILITHIUM5_CLEAN_unpack_pk, "_unpack_pk_jazz");
-    check_sk_rhotrkeyt0s1s2(PQCLEAN_DILITHIUM5_CLEAN_pack_sk_jazz, PQCLEAN_DILITHIUM5_CLEAN_pack_sk, "_pack_sk_jazz");
-    check_rhotrkeyt0s1s2_sk(PQCLEAN_DILITHIUM5_CLEAN_unpack_sk_jazz, PQCLEAN_DILITHIUM5_CLEAN_unpack_sk, "_unpack_sk_jazz");
-    check_sig_czh(PQCLEAN_DILITHIUM5_CLEAN_pack_sig_jazz, PQCLEAN_DILITHIUM5_CLEAN_pack_sig, "_pack_sig_jazz");
-    check_czh_sig(PQCLEAN_DILITHIUM5_CLEAN_unpack_sig_jazz, PQCLEAN_DILITHIUM5_CLEAN_unpack_sig, "_unpack_sig_jazz");
+    check_pk_rhot1(PQCLEAN_DILITHIUM5_CLEAN_pack_pk_jazz, PQCLEAN_DILITHIUM5_CLEAN_pack_pk, "pack_pk_jazz");
+    check_rhot1_pk(PQCLEAN_DILITHIUM5_CLEAN_unpack_pk_jazz, PQCLEAN_DILITHIUM5_CLEAN_unpack_pk, "unpack_pk_jazz");
+    check_sk_rhotrkeyt0s1s2(PQCLEAN_DILITHIUM5_CLEAN_pack_sk_jazz, PQCLEAN_DILITHIUM5_CLEAN_pack_sk, "pack_sk_jazz");
+    check_rhotrkeyt0s1s2_sk(PQCLEAN_DILITHIUM5_CLEAN_unpack_sk_jazz, PQCLEAN_DILITHIUM5_CLEAN_unpack_sk, "unpack_sk_jazz");
+    //    check_sig_czh(PQCLEAN_DILITHIUM5_CLEAN_pack_sig_jazz, PQCLEAN_DILITHIUM5_CLEAN_pack_sig, "pack_sig_jazz");
+    //    check_czh_sig(PQCLEAN_DILITHIUM5_CLEAN_unpack_sig_jazz, PQCLEAN_DILITHIUM5_CLEAN_unpack_sig, "unpack_sig_jazz");
     return 0;
 }
