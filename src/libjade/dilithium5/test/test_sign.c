6c6920b8eed55d1572b48a07ef777b673ff7d408
LE BRETON Come <come.le-breton@inria.fr>
2024-06-19 17:25:26 +0200

    Advance on signing.

---
+++ src/libjade/dilithium5/test/test_sign.c
@@ -0,0 +1,14 @@
+#include "notrandombytes.c"
+
+#include "test_macros.h"
+
+#ifndef TESTS
+#define TESTS 1000
+#endif
+
+int main ()
+{
+//     check_pk_rhot1(PQCLEAN_DILITHIUM5_CLEAN_pack_pk_jazz, PQCLEAN_DILITHIUM5_CLEAN_pack_pk, "pack_pk_jazz");
+
+    return 0;
+}
