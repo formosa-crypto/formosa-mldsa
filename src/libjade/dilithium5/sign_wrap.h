be3d1bbe2025b1d018061511097025ccc662de56
Côme Le Breton <come.le-breton@inria.fr>
2024-06-21 15:45:06 +0200

    Fix wrapping and tests.

--- src/libjade/dilithium5/sign_wrap.h
+++ src/libjade/dilithium5/sign_wrap.h
@@ -10,7 +10,7 @@ int PQCLEAN_DILITHIUM5_CLEAN_crypto_sign_verify_internal_jazz(const uint8_t *sig
 						     const uint8_t *pk);
 
 
-int PQCLEAN_DILITHIUM5_CLEAN_crypto_sign_signature_internal_jazz(uint8_t sig[PQCLEAN_DILITHIUM5_CLEAN_CRYPTO_SECRETKEYBYTES], int32_t* m,  int32_t siglen_mlen[2], uint8_t sk[PQCLEAN_DILITHIUM5_CLEAN_CRYPTO_SECRETKEYBYTES]);
+int PQCLEAN_DILITHIUM5_CLEAN_crypto_sign_signature_internal_jazz(uint8_t sig[PQCLEAN_DILITHIUM5_CLEAN_CRYPTO_BYTES], uint8_t* m,  size_t siglen_mlen[2], uint8_t sk[PQCLEAN_DILITHIUM5_CLEAN_CRYPTO_SECRETKEYBYTES]);
 
 int PQCLEAN_DILITHIUM5_CLEAN_crypto_sign_verify_jazz(const uint8_t *sig,
 						     size_t siglen,
@@ -26,9 +26,9 @@ int PQCLEAN_DILITHIUM5_CLEAN_crypto_sign_verify_jazz(const uint8_t *sig,
   return PQCLEAN_DILITHIUM5_CLEAN_crypto_sign_verify_internal_jazz(sig, m, mlen, pk);
 }
 
-int PQCLEAN_DILITHIUM5_CLEAN_crypto_sign_signature_jazz(uint8_t sig[PQCLEAN_DILITHIUM5_CLEAN_CRYPTO_SECRETKEYBYTES], int32_t *siglen, int32_t* m, int32_t mlen, uint8_t sk[PQCLEAN_DILITHIUM5_CLEAN_CRYPTO_SECRETKEYBYTES])
+int PQCLEAN_DILITHIUM5_CLEAN_crypto_sign_signature_jazz(uint8_t sig[PQCLEAN_DILITHIUM5_CLEAN_CRYPTO_SECRETKEYBYTES], size_t *siglen, uint8_t* m, size_t mlen, uint8_t sk[PQCLEAN_DILITHIUM5_CLEAN_CRYPTO_SECRETKEYBYTES])
 {
-  uint32_t siglen_mlen[2];
+  size_t siglen_mlen[2];
   siglen_mlen[0] = *siglen;
   siglen_mlen[1] = mlen;
 
