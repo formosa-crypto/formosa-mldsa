a842e89f0a6cbd8ed9f3b6a8fb34d43b3a649a5a
Côme Le Breton <come.le-breton@inria.fr>
2024-07-03 16:14:18 +0200

    Fix typo.

--- src/libjade/dilithium5/benchmark/j/main.c
+++ src/libjade/dilithium5/benchmark/j/main.c
@@ -13,7 +13,7 @@
 
 extern int PQCLEAN_DILITHIUM5_CLEAN_crypto_sign_keypair_seed(uint8_t *pk, uint8_t *sk, const uint8_t seed[SEEDBYTES]);
 extern int PQCLEAN_DILITHIUM5_CLEAN_crypto_sign_keypair(uint8_t *pk, uint8_t *sk);
-extern int PQCLEAN_DILITHIUM5_CLEAN_crypto_sign_signature(uint8_t sig[PQCLEAN_DILITHIUM5_CLEAN_CRYPTO_SECRETKEYBYTES], size_t *siglen, uint8_t* m, size_t mlen, uint8_t sk[PQCLEAN_DILITHIUM5_CLEAN_CRYPTO_SECRETKEYBYTES]);
+extern int PQCLEAN_DILITHIUM5_CLEAN_crypto_sign_signature(uint8_t sig[PQCLEAN_DILITHIUM5_CLEAN_CRYPTO_BYTES], size_t *siglen, uint8_t* m, size_t mlen, uint8_t sk[PQCLEAN_DILITHIUM5_CLEAN_CRYPTO_SECRETKEYBYTES]);
 extern int PQCLEAN_DILITHIUM5_CLEAN_crypto_sign_verify(const uint8_t *sig, size_t siglen, const uint8_t *m, size_t mlen, const uint8_t *pk);
 
 int main () {
