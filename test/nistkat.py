0a91a4f12ff467ef853e795248861442e6aa25fb
xvzcf <xvzcf@users.noreply.github.com>
2024-10-10 15:39:30 -0400

    Temporarily remove all non key-generation code.

--- test/nistkat.py
+++ test/nistkat.py
@@ -12,16 +12,16 @@ def generate_keypair(randomness):
     verification_key = ctypes.create_string_buffer(1952)
     signing_key = ctypes.create_string_buffer(4032)
 
-    ret = _ml_dsa._jade_sign_dilithium_dilithium3_amd64_ref_keypair_derand(verification_key, signing_key, bytearray_to_ctype(randomness))
+    ret = _ml_dsa.ml_dsa_65_generate_keypair(verification_key, signing_key, bytearray_to_ctype(randomness))
     assert ret == 0
 
     return (verification_key.raw, signing_key.raw)
 
 # ----- NIST KAT Tests -----
-with open("nistkats_6_5.json", "r") as nistkats_6_5_raw:
-    nistkats_6_5 = json.load(nistkats_6_5_raw)
+with open("nistkats_65.json", "r") as nistkats_65_raw:
+    nistkats_65 = json.load(nistkats_65_raw)
 
-    for nistkat in nistkats_6_5:
+    for nistkat in nistkats_65:
         key_generation_seed = bytearray.fromhex(nistkat['key_generation_seed'])
         (verification_key, signing_key) = generate_keypair(key_generation_seed)
 
