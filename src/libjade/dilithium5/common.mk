263b64cc8dfddbf0ece8d01a48d9efc15fea1c83
LE BRETON Come <come.le-breton@inria.fr>
2024-10-15 11:32:56 +0200

    Advance on modularization.

--- src/libjade/dilithium5/common.mk
+++ src/libjade/dilithium5/common.mk
@@ -1,18 +1,18 @@
 # Configure the target architecture and which compiler to use assemble
 ifeq ($(arch), risc-v)
-	ARMCC ?= riscv64-unknown-linux-gnu-gcc
-	ARMCFLAGS ?= -Wall -O1 -march=rv32im -mabi=ilp32
-	ARMAR ?= riscv64-unknown-linux-gnu-ar
+	CC ?= riscv64-unknown-linux-gnu-gcc
+	CFLAGS ?= -Wall -O1 -march=rv32im -mabi=ilp32
 
 	UNAME_S := $(shell uname -s)
 	ifeq ($(UNAME_S),Darwin)
-		ARMCC := /opt/homebrew/bin/riscv64-unknown-elf-gcc
-		ARMCFLAGS := -Wall -O1 -march=rv32im -mabi=ilp32
-		ARMAR := /opt/homebrew/bin/riscv64-unknown-elf-ar
+		CC := /opt/homebrew/bin/riscv64-unknown-elf-gcc
+		CFLAGS := -Wall -O1 -march=rv32im -mabi=ilp32
+		AR := /opt/homebrew/bin/riscv64-unknown-elf-ar
 	endif
 else ifeq ($(arch), arm-m4)
-	ARMCC := arm-none-eabi-gcc
-	ARMCFLAGS := -Wall -O1 -fPIC -mcpu=cortex-m4 -mlittle-endian -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 --specs=nosys.specs
+	CC := arm-none-eabi-gcc
+	CFLAGS := -Wall -O1 -fPIC -mcpu=cortex-m4 -mlittle-endian -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 --specs=nosys.specs
+	AR ?= riscv64-unknown-linux-gnu-ar
 	#-mcpu=cortex-m4 # not working - TODO: check why (same for test/)
 else
 	$(error arch must be set to risc-v or arm-m4)
