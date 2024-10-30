06ecc2cb47c41a2e40b89678eae1b57d3b8aff58
LE BRETON Come <come.le-breton@inria.fr>
2024-10-30 14:29:26 +0100

    WIP Copy from 65 to 44, with some renaming

---
+++ src/libjade/ml-dsa-44/test/test_packing.c
@@ -0,0 +1,82 @@
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "params.h"
+#include "poly.h"
+#include "polyvec.h"
+#include "packing.h"
+#include "notrandombytes.c"
+#include "fips202.h"
+#include "test_macros.h"
+
+#ifndef TESTS
+#define TESTS 1000
+#endif
+
+#include "packing_wrap.h"
+
+void JASMIN_MLDSA44_pack_pk(uint8_t pk[JASMIN_MLDSA44_CRYPTO_PUBLICKEYBYTES], const uint8_t rho[SEEDBYTES], const polyveck *t1);
+void JASMIN_MLDSA44_unpack_pk(uint8_t rho[SEEDBYTES], polyveck *t1, const uint8_t pk[JASMIN_MLDSA44_CRYPTO_PUBLICKEYBYTES]);
+void JASMIN_MLDSA44_pack_sig(uint8_t sig[JASMIN_MLDSA44_CRYPTO_BYTES], const uint8_t c[CTILDEBYTES], const polyvecl *z, const polyveck *h);
+int JASMIN_MLDSA44_unpack_sig(uint8_t c[CTILDEBYTES], polyvecl *z, polyveck *h, const uint8_t sig[JASMIN_MLDSA44_CRYPTO_BYTES]);
+
+void check_pack_sig() {
+  int32_t z[N*L];
+  int32_t z_jazz[N*L];
+  int32_t h[N*K];
+  int32_t h_jazz[N*K];
+  uint8_t c[CTILDEBYTES];
+  uint8_t c_jazz[CTILDEBYTES];
+  uint8_t sig[JASMIN_MLDSA44_CRYPTO_BYTES];
+  uint8_t sig_jazz[JASMIN_MLDSA44_CRYPTO_BYTES];
+  for(int t=0; t<TESTS; t++) {
+    fillarrni32(z, z_jazz, N*L);
+    fillarrni32(h, h_jazz, N*K);
+    for (int i = 0; i < K * N; ++i) {
+      h[i] = 0;
+      h_jazz[i] = 0;
+    }
+    fillarrnu8(c, c_jazz, CTILDEBYTES);
+    JASMIN_MLDSA44_pack_sig(sig, c, (polyvecl *)z, (polyveck *)h);
+    JASMIN_MLDSA44_pack_sig(sig_jazz, c, (polyvecl *)z_jazz, (polyveck *)h_jazz);
+    eqarr(JASMIN_MLDSA44_CRYPTO_BYTES, PRId8, sig, sig_jazz, "pack_sig")
+  }
+  printf("PASS: pack_sig\n");
+ }
+
+void check_unpack_sig() {
+  int32_t z[N*L];						 
+  int32_t z_jazz[N*L];
+  int32_t h[N*K];
+  int32_t h_jazz[N*K];
+  uint8_t c[CTILDEBYTES];
+  uint8_t c_jazz[CTILDEBYTES];
+  uint8_t sig[JASMIN_MLDSA44_CRYPTO_BYTES];
+  uint8_t sig_jazz[JASMIN_MLDSA44_CRYPTO_BYTES];
+  for(int t=0; t<TESTS; t++) {
+    fillarrnu8(sig, sig_jazz, JASMIN_MLDSA44_CRYPTO_BYTES);
+    int r = JASMIN_MLDSA44_unpack_sig(c, (polyvecl *)z, (polyveck *)h, sig);
+    int r_jazz = JASMIN_MLDSA44_unpack_sig(c_jazz, (polyvecl *)z_jazz, (polyveck *)h_jazz, sig_jazz);
+    if (r != r_jazz) {
+      printf("FAIL: unpack_sig\n");
+      exit(1);
+    }
+    if (r == 0) {
+      eqarr(N*L, PRId32, z, z_jazz, "unpack_sig");
+      eqarr(N*K, PRId32, h, h_jazz, "unpack_sig");
+    }
+  }
+  printf("PASS: unpack_sig_jazz\n");
+}
+
+int main ()
+{
+     check_pk_rhot1(JASMIN_MLDSA44_pack_pk, JASMIN_MLDSA44_pack_pk, "pack_pk_jazz");
+     check_rhot1_pk(JASMIN_MLDSA44_unpack_pk, JASMIN_MLDSA44_unpack_pk, "unpack_pk_jazz");
+     check_sk_rhotrkeyt0s1s2(JASMIN_MLDSA44_pack_sk, JASMIN_MLDSA44_pack_sk, "pack_sk_jazz");
+     check_rhotrkeyt0s1s2_sk(JASMIN_MLDSA44_unpack_sk, JASMIN_MLDSA44_unpack_sk, "unpack_sk_jazz");
+     check_pack_sig();
+     check_unpack_sig();
+    return 0;
+}
---
+++ src/libjade/ml-dsa-44/test/test_sign.c
@@ -0,0 +1,141 @@
+#include "notrandombytes.c"
+
+#include "test_macros.h"
+
+#include "sign.h"
+#include "fips202.h"
+
+#ifndef TESTS
+#define TESTS 5
+#endif
+
+void JASMIN_MLDSA44_crypto_sign_keypair_seed(uint8_t *pk, uint8_t *sk, uint8_t *sb);
+void shake256_PUBLICKEYBYTES_SEEDBYTES(uint8_t *output, const uint8_t *input);
+
+#include "sign_wrap.h"
+
+void check_crypto_sign_keypair_seed() {
+  uint8_t pk[PQCLEAN_MLDSA44_CLEAN_CRYPTO_PUBLICKEYBYTES];
+  uint8_t pk_jazz[PQCLEAN_MLDSA44_CLEAN_CRYPTO_PUBLICKEYBYTES];
+  uint8_t sk[PQCLEAN_MLDSA44_CLEAN_CRYPTO_SECRETKEYBYTES];
+  uint8_t sk_jazz[PQCLEAN_MLDSA44_CLEAN_CRYPTO_SECRETKEYBYTES];
+  uint8_t sb[2 * SEEDBYTES + CRHBYTES];
+  uint8_t sb_jazz[2 * SEEDBYTES + CRHBYTES];
+  for (int t=0; t<TESTS; t++) {
+    fillarrnu8(sb, sb_jazz, 2 * SEEDBYTES + CRHBYTES);
+    PQCLEAN_MLDSA44_CLEAN_crypto_sign_keypair_seed(pk, sk, sb);
+    JASMIN_MLDSA44_crypto_sign_keypair_seed(pk_jazz, sk_jazz, sb_jazz);
+    eqarr(PQCLEAN_MLDSA44_CLEAN_CRYPTO_PUBLICKEYBYTES, PRId8, pk, pk_jazz, "crypto_sign_keypair_seed: wrong pk");
+    eqarr(PQCLEAN_MLDSA44_CLEAN_CRYPTO_SECRETKEYBYTES, PRId8, sk, sk_jazz, "crypto_sign_keypair_seed: wrong sk");
+  }
+  printf("PASS: crypto_sign_keypair_seed\n");
+}
+
+void check_shake256_PUBLICKEYBYTES_SEEDBYTES() {
+  uint8_t outp[SEEDBYTES];
+  uint8_t outp_jazz[SEEDBYTES];
+  uint8_t inp[PQCLEAN_MLDSA44_CLEAN_CRYPTO_PUBLICKEYBYTES];
+  uint8_t inp_jazz[PQCLEAN_MLDSA44_CLEAN_CRYPTO_PUBLICKEYBYTES];
+  for (int t=0; t<TESTS; t++) {
+    fillarrnu8(inp, inp_jazz, PQCLEAN_MLDSA44_CLEAN_CRYPTO_PUBLICKEYBYTES);
+    shake256(outp, SEEDBYTES, inp, PQCLEAN_MLDSA44_CLEAN_CRYPTO_PUBLICKEYBYTES);
+    shake256_PUBLICKEYBYTES_SEEDBYTES(outp_jazz, inp_jazz);
+    eqarr(SEEDBYTES, PRId8, outp, outp_jazz, "shake256_PUBLICKEYBYTES_SEEDBYTES");
+  }
+  printf("PASS: shake256_PUBLICKEYBYTES_SEEDBYTES\n");
+}
+
+void check_crypto_sign_signature_ctx() {
+  size_t siglen = 0;
+  size_t siglen_jazz = 0;
+  size_t mlen = 453;
+  size_t ctxlen = 239;
+  uint8_t sig[PQCLEAN_MLDSA44_CLEAN_CRYPTO_BYTES];
+  uint8_t sig_jazz[PQCLEAN_MLDSA44_CLEAN_CRYPTO_BYTES];
+  uint8_t m[mlen];
+  uint8_t m_jazz[mlen];
+  uint8_t ctx[ctxlen];
+  uint8_t ctx_jazz[ctxlen];
+  uint8_t pk[PQCLEAN_MLDSA44_CLEAN_CRYPTO_PUBLICKEYBYTES];
+  uint8_t sk[PQCLEAN_MLDSA44_CLEAN_CRYPTO_SECRETKEYBYTES];
+  uint8_t sb[2 * SEEDBYTES + CRHBYTES];
+  for(int t=0; t<TESTS; t++) {
+    // Generates a keypair from random seed    
+    fillarr(uint8_t, 2 * SEEDBYTES + CRHBYTES, sb);
+    int status = PQCLEAN_MLDSA44_CLEAN_crypto_sign_keypair_seed(pk, sk, sb);
+    if (status != 0) {
+      printf("FAIL: crypto_sign_signature\n. Could not generate the key pair.");
+      exit(1);
+    }
+    // Generates random message
+    fillarrnu8(m, m_jazz, mlen);
+    // Generates random context
+    fillarrnu8(ctx, ctx_jazz, ctxlen);
+    // Sign the random message
+    int r = PQCLEAN_MLDSA44_CLEAN_crypto_sign_signature_ctx(sig, &siglen, m, mlen, ctx, ctxlen, sk);
+    if (r != 0) {
+      printf("FAIL: crypto_sign_signature. Could not generate the signature with C code.");
+      exit(1);
+    }
+    int r_jazz = JASMIN_MLDSA44_crypto_sign_signature_ctx(sig_jazz, &siglen_jazz, m_jazz, mlen, ctx_jazz, ctxlen, sk);
+    if (r_jazz != 0) {
+      printf("FAIL: crypto_sign_signature. Could not generate the signature with jasmin code.");
+      exit(1);
+    }
+    eqarr(PQCLEAN_MLDSA44_CLEAN_CRYPTO_BYTES, PRId8, sig, sig_jazz, "crypto_sign_signature_ctx");
+  }
+  printf("PASS: crypto_sign_signature\n");
+}
+
+void check_crypto_sign_verify_ctx() {
+  size_t siglen = PQCLEAN_MLDSA44_CLEAN_CRYPTO_BYTES;
+  size_t mlen = 453;
+  size_t ctxlen = 239;
+  uint8_t m[mlen];
+  uint8_t ctx[ctxlen];
+  uint8_t sig[PQCLEAN_MLDSA44_CLEAN_CRYPTO_BYTES];
+  uint8_t pk[PQCLEAN_MLDSA44_CLEAN_CRYPTO_PUBLICKEYBYTES];
+  uint8_t sk[PQCLEAN_MLDSA44_CLEAN_CRYPTO_SECRETKEYBYTES];
+  uint8_t sb[2 * SEEDBYTES + CRHBYTES];  
+  for(int t=0; t<TESTS; t++) {
+    // Generates a keypair from random seed
+    fillarr(uint8_t, 2 * SEEDBYTES + CRHBYTES, sb);
+    int status = PQCLEAN_MLDSA44_CLEAN_crypto_sign_keypair_seed(pk, sk, sb);
+    if (status != 0) {
+      printf("FAIL: crypto_sign_verify. Could not generate the key pair.\n");
+      exit(1);
+    }
+    // Generates random message
+    fillarr(uint8_t, mlen, m);
+    // Generates random context
+    fillarr(uint8_t, ctxlen, ctx);
+    // Sign the random message
+    status = PQCLEAN_MLDSA44_CLEAN_crypto_sign_signature_ctx(sig, &siglen, m, mlen, ctx, ctxlen, sk);
+    if (status != 0) {
+      printf("FAIL: crypto_sign_verify. Could not generate the signature.\n");
+      exit(1);
+    }
+    // Verify the maching between the signature and the message
+    int r = PQCLEAN_MLDSA44_CLEAN_crypto_sign_verify_ctx(sig, PQCLEAN_MLDSA44_CLEAN_CRYPTO_BYTES, m, mlen, ctx, ctxlen, pk);
+    if (r != 0) {
+      printf("Could not verify signature with C code.");
+      exit(1);
+    }
+    int r_jazz = JASMIN_MLDSA44_crypto_sign_verify_ctx(sig, PQCLEAN_MLDSA44_CLEAN_CRYPTO_BYTES, m, mlen, ctx, ctxlen, pk);
+    if (r != r_jazz) {
+      printf("FAIL: crypto_sign_verify\n");
+      exit(1);
+    }
+  }
+  printf("PASS: crypto_sign_verify\n");
+}
+
+int main ()
+{
+  check_shake256_PUBLICKEYBYTES_SEEDBYTES();
+  check_crypto_sign_keypair_seed();
+  check_crypto_sign_signature_ctx();
+  check_crypto_sign_verify_ctx();
+
+  return 0;
+}
