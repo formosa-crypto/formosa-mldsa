b5e76daaf604b0aa37beb40bb848cb1a9ba3b83b
xvzcf <xvzcf@users.noreply.github.com>
2024-12-17 15:44:23 -0500

    Small changes.

--- rsb-checker.nix
+++ rsb-checker.nix
@@ -1,13 +1,13 @@
-with (import <nixpkgs> {});
 let
+    pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/6dbbd5a2622d9abf9263a5cb0a85ca218974b085.tar.gz") {};
     repo =  builtins.fetchGit {
             url = "https://github.com/sarranz/jasmin";
             ref = "rsb3";
             rev = "324715a088a48b2a43bb3a22aafccdfd003992a2";
     };
 
-    jasmin-rsb = import (repo + "/default.nix") { };
+    jasmin-rsb = import (repo + "/default.nix") {};
 in
-mkShell {
+pkgs.mkShell {
     buildInputs = [ jasmin-rsb ];
 }
